611776104171d4ecdf53f08d64f8b4c6ef075eb7
RiccardoImperato <riccardo.imperato@libero.it>
2023-12-07 23:50:06 +0100

    feat: componente banner con icone

--- src/components/AppBanner.vue
+++ src/components/AppBanner.vue
@@ -5,7 +5,30 @@ export default {}
 <template>
     <div class="banner">
         <div class="container">
-            Banner
+            <ul>
+                <li>
+                    <img src="/img/buy-comics-digital-comics.png">
+                    <span>
+                        Digital comics
+                    </span>
+                </li>
+                <li>
+                    <img src="/img/buy-comics-merchandise.png">
+                    <span>Dc merchandise</span>
+                </li>
+                <li>
+                    <img src="/img/buy-comics-subscriptions.png">
+                    <span>Subscription</span>
+                </li>
+                <li>
+                    <img src="/img/buy-comics-shop-locator.png">
+                    <span>Comic Shop locator</span>
+                </li>
+                <li>
+                    <img src="/img/buy-dc-power-visa.svg">
+                    <span>Dc power visa</span>
+                </li>
+            </ul>
         </div>
     </div>
 </template>
@@ -17,5 +40,36 @@ export default {}
     background-color: $banner-bg;
     color: #fff;
     padding: 50px 0;
+
+    .container {
+        padding: 0 40px;
+
+        ul {
+            all: unset;
+            list-style-type: none;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+        }
+
+        li {
+            display: flex;
+            align-items: center;
+            font-size: 12px;
+            text-transform: uppercase;
+
+            img {
+                object-fit: contain;
+                width: 50px;
+                height: 50px;
+            }
+
+            span {
+                margin-left: 15px;
+            }
+        }
+    }
+
+
 }
 </style>
